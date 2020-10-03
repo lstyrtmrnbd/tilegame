3ec97423825147b7cf4fff1aaf30d115118827c4
Mule Sknr <soymilk@airmail.cc>
2020-10-03 16:01:21 -0400

    Render fn wrapped

--- preload.js
+++ preload.js
@@ -7,10 +7,10 @@ const {
     setEach
 } = require('./board');
 
-// need for this?
+// a need for this?
 // process.env["NODE_OPTIONS"] = '--no-force-async-hooks-checks';
 
-const BOARD = newBoard(64,64);
+const BOARD = newBoard(64,64); // 4096 tiles
 setEach(() => Math.round(Math.random() * 149))(BOARD);    
 
 // does set-up and returns the actual rendering fn
@@ -34,12 +34,20 @@ const renderBoard = async () => {
 window.addEventListener('DOMContentLoaded', async () => {
 
     // 32ms interval: ~30fps
-    const primary = seedEngine(32); 
+    // 16ms interval: ~60fps
+    const primary = seedEngine(2);
+
+    const boardRender = await renderBoard();
     
-    await primary(await renderBoard(), BOARD);
+    const wrapped = async (BOARD) => {
+        console.log(`wrapper was called @${Math.round(performance.now() / 1000)}`);
+        boardRender(BOARD);
+    };
+    
+    await primary(wrapped, BOARD);
 });
 
 // produce async engine call at specified interval
 const seedEngine = interval => async (engine, ...args) => {
     setInterval(engine, interval, ...args);
-}
+};
